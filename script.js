3aa5acbcd1d784a5cf5b37314b36e2cd6f3cc451
Evander Oktapian <evandertambunan@gmail.com>
2023-08-15 15:48:00 +0700

    mantap dijiwa brader

---
+++ script.js
@@ -0,0 +1,58 @@
+$(".search-button").on("click", function () {
+  $.ajax({
+    url: "http://www.omdbapi.com/?apikey=dca61bcc&s=" + $(".input-keyword").val(),
+    success: (result) => {
+      const movies = result.Search;
+      let cards = "";
+      movies.forEach((element) => {
+        cards += tambahCard(element);
+      });
+      $(".movie-container").html(cards);
+
+      // ketika tombol detail diklik
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url: "http://www.omdbapi.com/?apikey=dca61bcc&i=" + $(this).data("imdbid"),
+          success: (element) => {
+            const movieDetail = tampilMovieDetail(element);
+            $(".modal-body").html(movieDetail);
+          },
+        });
+      });
+    },
+    error: () => {
+      console.log(e.responseText);
+    },
+  });
+});
+
+function tambahCard(element) {
+  return `<div class="col-md-4 my-3">
+                <div class="card" >
+                  <img src="${element.Poster}" class="card-img-top"/>
+                  <div class="card-body">
+                    <h5 class="card-title">${element.Title}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">${element.Year}</h6>
+                    <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#detailFilmModal" data-imdbid="${element.imdbID}">Detail</a>
+                  </div>
+                </div>
+</div>`;
+}
+
+function tampilMovieDetail(element) {
+  return `<div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-3">
+                    <img src="${element.Poster}" alt="" class="img-fluid" />
+                  </div>
+                  <div class="col-md">
+                    <ul class="list-group">
+                      <li class="list-group-item"><h4>${element.title} (${element.Year})</h4></li>
+                      <li class="list-group-item">Director: ${element.Director}</li>
+                      <li class="list-group-item">Actors: ${element.actors}</li>
+                      <li class="list-group-item">Plot : <br />${element.Plot}</li>
+                    </ul>
+                  </div>
+            </div>
+</div>`;
+}
